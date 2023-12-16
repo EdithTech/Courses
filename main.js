035ba54d9d885e9db4e689a53d00bf7ef35b6b96
EdithTech <yash9atalkar@gmail.com>
2023-12-16 20:07:06 +0530

    upadat

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 // change navbar on scroll
 
 window.addEventListener("scroll", () => {
-    document.querySelector("nav").classList.toggle('window-scroll', window.scrollY > 0)
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+const faqs = document.querySelectorAll(".faq");
+
+console.log(faqs);
+
+// faqs[0].addEventListener("click", function(){ alert("Hello World!"); })
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    console.log("clicked");
+    faq.classList.toggle("open");
+
+    // change icon
+    const icon = faq.querySelector(".faq__icon i");
+
+    if (icon.className === "fa-solid fa-plus") {
+      icon.className = "fa-solid fa-minus";
+    } else {
+      icon.className = "fa-solid fa-plus";
+    }
+  });
 });
